c2024f37febc68abc41cfc3e74bed5d35bbd9b93
Thomas Visco <thomasvisco@gmail.com>
2018-06-14 01:25:09 -0500

    Cleaned up.

--- html_test.py
+++ html_test.py
@@ -1,22 +1,14 @@
 
-import bs4 as BeautifulSoup
 from bs4 import BeautifulSoup
-import html.parser
 
 html = open('/home/roc/dev/acc_python/reddit-crawl-beautifulsoup/reddit.html')
 
 soup = BeautifulSoup(html, 'html.parser')
 
-print(soup)
+main_table = soup.find('div', attrs={'class':'entry'})
 
-#main_table = soup.find('div', attrs={'class':'entry'})
+links = main_table.find('p', attrs={'class':'title may-blank outbound'})    #not working
 
-#print(main_table)
+title = soup.find('p', attrs={'class': 'title'})
 
-#links = main_table.find('p', attrs={'class':'title may-blank outbound'})
-
-#print(links)
-
-#title = soup.find('p', attrs={'class': 'title'})
-
-#print(title)
+print(title)
